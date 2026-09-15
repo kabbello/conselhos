b0a4f290d11bf38d79fb8258655dbac581a6e69e
kabbello <kabbello@gmail.com>
2026-09-15 09:33:27 -0300

    fix: R2 endpoint EU + path-style obrigatório

--- config/filesystems.php
+++ config/filesystems.php
@@ -68,7 +68,7 @@ return [
             'bucket' => env('R2_BUCKET'),
             'url' => env('R2_URL'),
             'endpoint' => env('R2_ENDPOINT'),
-            'use_path_style_endpoint' => false,
+            'use_path_style_endpoint' => true,
             'visibility' => 'public',
             'throw' => true,
             'report' => false,
